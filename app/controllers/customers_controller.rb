66729b198c8ca65729086a6952a0da83ede72d17
Yirui-Huang227 <huangyr227@gmail.com>
2026-03-05 19:38:50 -0600

    Create index, alphabetized, missing_email action/view

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,4 +1,13 @@
-    # Only allow a list of trusted parameters through.
-    def customer_params
-      params.expect(customer: [ :full_name, :phone_number, :email_address, :notes, :image ])
-    end
+class CustomersController < ApplicationController
+  def index
+    @customers = Customer.all
+  end
+
+  def alphabetized
+    @customers = Customer.order(:full_name)
+  end
+
+  def missing_email
+    @customers = Customer.where(email_address: [nil, ""])
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  root to: 'customers#index'
+
+  get 'customers/alphabetized', to: 'customers#alphabetized', as: :customers_alphabetized
+  get 'customers/missing_email', to: 'customers#missing_email', as: :customers_missing_email
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
